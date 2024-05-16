13bbdcc7a42146d2405af9e3b27bc8611247c073
hooke003 <91613022+hooke003@users.noreply.github.com>
2024-05-16 12:19:31 -0500

    working connection to appwrite

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -2,17 +2,17 @@ import { ID } from "appwrite";
 import { INewUser } from "@/types";
 import { account } from "./config";
 
-export async function createUserAccount(user: INewUser) {
+export async function createUserAccount(user: INewUser): Promise<any> {
   try {
     const newAccount = await account.create(
-        ID.unique,
+        ID.unique(),
         user.email,
         user.password,
         user.name
-    )
+    );
     return newAccount;
   } catch (error) {
-    console.log(error);
-    return error;
+    console.error("Error creating user account:", JSON.stringify(error, null, 2));
+    return { success: false, error: error };
   }
 }
